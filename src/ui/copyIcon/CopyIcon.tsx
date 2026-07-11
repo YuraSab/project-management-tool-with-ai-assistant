510128c8cc6d8f008d49bf1fad5c1b8d79be7fd4
YuraSab <yrasabat@gmail.com>
2026-07-11 13:16:27 +0300

    feat: copy user email

--- src/ui/copyIcon/CopyIcon.tsx
+++ src/ui/copyIcon/CopyIcon.tsx
@@ -2,21 +2,27 @@ import React from 'react';
 import {copyText} from "../../utils/copy.ts";
 import {Copy} from "lucide-react";
 import styles from './CopyIcon.module.css';
+import {toast} from "../../utils/toaster.ts";
 
 interface CopyIcon {
-    text: string,
+    copyValue: string,
+    toastValue?: string,
+    size?: number,
+    customStyles: React.CSSProperties,
 }
 
-const CopyIcon = ({ text }: CopyIcon) => {
+const CopyIcon = ({ copyValue, toastValue, size = 19, customStyles }: CopyIcon) => {
     const handleCopy = () => {
-        copyText(text);
+        copyText(copyValue);
+        toast.info(toastValue ?? 'Copied!');
     };
 
     return (
         <Copy
-            size={19}
+            size={size}
             onClick={handleCopy}
             className={styles.copyIcon}
+            style={{ ...customStyles }}
         />
     );
 };
